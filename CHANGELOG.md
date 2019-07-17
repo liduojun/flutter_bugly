f19ef1926ae700f596e0f9ed10f605526727f0c1
liduojun <361848645@qq.com>
2019-07-17 09:52:08 +0800

    1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,38 @@
+## 0.2.3+1
+* 修复useCache取值不正确的问题
+## 0.2.3
+* 优化checkUpgrade方法，等待网络请求更新策略完成后再返回UpgradeInfo（注意点见方法注释）
+* 升级com.tencent.bugly:crashreport_upgrade:1.4.1
+## 0.2.2
+* 新增初始化channel参数，见注释
+* 新增setUserTag、putUserData方法，见注释
+## 0.2.1+1
+* 新增setUserId方法，用于crash用户标识
+## 0.2.0+1
+* 优化UpgradeInfo获取机制，优先获取网络策略，网络策略没有来得及拉取时，取本地策略
+## 0.2.0
+* 优化UpgradeInfo获取机制
+## 0.1.9+2
+* 解决The 'Pods-Runner' target has transitive dependencies that include static binaries
+## 0.1.9+1
+* 获取更新信息增加非空判断
+## 0.1.9
+* 解决Android 9.0上联网失败的问题
+## 0.1.8
+* 增加异常上报过滤
+## 0.1.7
+* 优化格式化异常信息上报时的逻辑
+## 0.1.6+2
+* 修改iOS为返回InitResultInfo实体
+## 0.1.6+1
+* 删除.idea及优化逻辑严谨性
+## 0.1.6
+* sdk自带6.0以上动态权限，删除插件中的权限动态申请
+* 修改为返回InitResultInfo实体
+## 0.1.5+1
+* 升级 crashreport_upgrade:1.3.7，适配8.0通知栏，以及androidx的应用升级弹窗说明
+## 0.1.4+2
+* fix crash when FileProvider not find 
+## 0.1.4+1
+* migrate to androidx
+
---
+++ lib/src/flutter_bugly.dart
@@ -0,0 +1,161 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
+import 'bean/upgrade_info.dart';
+import 'bean/init_result_info.dart';
+
+class FlutterBugly {
+  FlutterBugly._();
+
+  static const MethodChannel _channel =
+      const MethodChannel('crazecoder/flutter_bugly');
+
+  ///初始化
+  static Future<InitResultInfo> init({
+    String androidAppId,
+    String iOSAppId,
+    String channel, //自定义渠道标识
+    bool autoCheckUpgrade = true,
+    bool autoInit = true,
+    bool autoDownloadOnWifi = false,
+    bool enableHotfix = false,
+    bool enableNotification = false, //未适配androidx
+    bool showInterruptedStrategy = true, //设置开启显示打断策略
+    bool canShowApkInfo = true, //设置是否显示弹窗中的apk信息
+    int initDelay = 0, //延迟初始化,单位秒
+    int upgradeCheckPeriod = 60, //升级检查周期设置,单位秒
+  }) async {
+    assert((Platform.isAndroid && androidAppId != null) ||
+        (Platform.isIOS && iOSAppId != null));
+    Map<String, Object> map = {
+      "appId": Platform.isAndroid ? androidAppId : iOSAppId,
+      "channel": channel,
+      "autoCheckUpgrade": autoCheckUpgrade,
+      "autoDownloadOnWifi": autoDownloadOnWifi,
+      "enableHotfix": enableHotfix,
+      "enableNotification": enableNotification,
+      "showInterruptedStrategy": showInterruptedStrategy,
+      "canShowApkInfo": canShowApkInfo,
+      "initDelay": initDelay,
+      "upgradeCheckPeriod": upgradeCheckPeriod,
+    };
+    final String result = await _channel.invokeMethod('initBugly', map);
+    Map resultMap = json.decode(result);
+    var resultBean = InitResultInfo.fromJson(resultMap);
+    return resultBean;
+  }
+
+  ///设置用户标识
+  static Future<Null> setUserId(String userId) async {
+    Map<String, Object> map = {
+      "userId": userId,
+    };
+    await _channel.invokeMethod('setUserId', map);
+  }
+
+  ///设置标签
+  ///userTag 标签ID，可在网站生成
+  static Future<Null> setUserTag(int userTag) async {
+    Map<String, Object> map = {
+      "userTag": userTag,
+    };
+    await _channel.invokeMethod('setUserTag', map);
+  }
+
+  ///设置关键数据，随崩溃信息上报
+  static Future<Null> putUserData(
+      {@required String key, @required String value}) async {
+    assert(key != null && key.isNotEmpty);
+    assert(value != null && value.isNotEmpty);
+    Map<String, Object> map = {
+      "key": key,
+      "value": value,
+    };
+    await _channel.invokeMethod('putUserData', map);
+  }
+
+  ///获取本地更新策略，即上次未更新的策略
+  static Future<UpgradeInfo> getUpgradeInfo() async {
+    final String result = await _channel.invokeMethod('getUpgradeInfo');
+    var info = _decodeUpgradeInfo(result);
+    return info;
+  }
+
+  ///检查更新
+  ///return 更新策略信息
+  static Future<UpgradeInfo> checkUpgrade({
+    bool isManual = false,
+    bool isSilence = false,
+    bool useCache = true,
+  }) async {
+    if (!Platform.isAndroid) return null;
+    Map<String, Object> map = {
+      "isManual": isManual, //用户手动点击检查，非用户点击操作请传false
+      "isSilence": isSilence, //是否显示弹窗等交互，[true:没有弹窗和toast] [false:有弹窗或toast]
+      "useCache": useCache, //是否使用第一次缓存的更新策略，false为实时的，但是bugly会可能返回null
+    };
+    final String result = await _channel.invokeMethod('checkUpgrade', map);
+    var info = _decodeUpgradeInfo(result);
+    return info;
+  }
+
+  ///异常上报
+  static void postCatchedException<T>(
+    T callback(), {
+    bool useLog = false, //是否打印，默认不打印异常
+    FlutterExceptionHandler handler, //异常捕捉，用于自定义打印异常
+    String filterRegExp, //异常上报过滤正则，针对message
+  }) {
+    var map = {};
+    // This captures errors reported by the Flutter framework.
+    FlutterError.onError = (FlutterErrorDetails details) async {
+      if (useLog || handler != null) {
+        // In development mode simply print to console.
+        handler == null
+            ? FlutterError.dumpErrorToConsole(details)
+            : handler(details);
+      } else {
+        Zone.current.handleUncaughtError(details.exception, details.stack);
+      }
+    };
+
+    // This creates a [Zone] that contains the Flutter application and stablishes
+    // an error handler that captures errors and reports them.
+    //
+    // Using a zone makes sure that as many errors as possible are captured,
+    // including those thrown from [Timer]s, microtasks, I/O, and those forwarded
+    // from the `FlutterError` handler.
+    //
+    // More about zones:
+    //
+    // - https://api.dartlang.org/stable/1.24.2/dart-async/Zone-class.html
+    // - https://www.dartlang.org/articles/libraries/zones
+    runZoned<Future<Null>>(() async {
+      callback();
+    }, onError: (error, stackTrace) async {
+      var errorStr = error.toString();
+      //异常过滤
+      if (filterRegExp != null) {
+        RegExp reg = new RegExp(filterRegExp);
+        Iterable<Match> matches = reg.allMatches(errorStr);
+        if (matches.length > 0) {
+          return;
+        }
+      }
+      map.putIfAbsent("crash_message", () => errorStr);
+      map.putIfAbsent("crash_detail", () => stackTrace.toString());
+      await _channel.invokeMethod('postCatchedException', map);
+    });
+  }
+
+  static UpgradeInfo _decodeUpgradeInfo(String jsonStr) {
+    if (jsonStr == null || jsonStr.isEmpty) return null;
+    Map resultMap = json.decode(jsonStr);
+    var info = UpgradeInfo.fromJson(resultMap);
+    return info;
+  }
+
+}
